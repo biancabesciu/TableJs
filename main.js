7f9f50e52deb3fa3269e5d44afe6f55584e7bda8
Bianca Besciu <bianca.besciu@gmail.com>
2018-05-25 14:15:09 +0200

    changed functions so that code won't repeat

--- main.js
+++ main.js
@@ -46,49 +46,8 @@ let tableArray = [
     }
 ];
 
-function init () {
-    for(let i = 0; i < tableArray.length; i++) {
-
-        let backgroundColor;
-        let pet = tableArray[i].pet;
-
-        if(pet === 'Cat') {
-            backgroundColor = "aliceBlue";
-        } else if (pet === 'Giraffe') {
-            backgroundColor = "#4d4c59";
-        } else if (pet === 'Dog'){
-            backgroundColor = "#71777d";
-        } else if (pet === 'Horse') {
-            backgroundColor = "#f7f6f0";
-        }
-
-        let table =
-            '<tr class="change-color" style ="background-color: ' + backgroundColor + '">' +
-            '<td class="pet">' + tableArray[i].pet + '</td>' +
-            '<td class="name">' + tableArray[i].name + '</td>' +
-            '<td class="color">' + tableArray[i].color+ '</td>' +
-            '<td class="gender">' + tableArray[i].gender + '</td>' +
-            '</tr>';
-
-        let tds = document.getElementsByTagName('td');
-        for(let i = 0; i < tds.length; i++) {
-            tds[i].contentEditable = 'true';
-        }
-
-        document.getElementById('tableBody').insertAdjacentHTML("beforeend", table);
-    }
-}
-document.addEventListener('DOMContentLoaded', init);
-
-function addRow() {
+function insertRow(rowObj) {
     let table = document.getElementById('tableBody');
-    let tr = document.getElementsByTagName('tr');
-    let td = document.getElementsByTagName('td');
-
-    let pet = document.getElementById('pet').value;
-    let name = document.getElementById('name').value;
-    let color = document.getElementById('color').value;
-    let gender = document.getElementById('gender').value;
 
     let newRow = table.insertRow(0);
 
@@ -97,20 +56,28 @@ function addRow() {
     let cel3 = newRow.insertCell(2);
     let cel4 = newRow.insertCell(3);
 
-    cel1.innerHTML = pet;
-    cel2.innerHTML = name;
-    cel3.innerHTML = color;
-    cel4.innerHTML = gender;
+    cel1.innerHTML = rowObj.pet;
+    cel2.innerHTML = rowObj.name;
+    cel3.innerHTML = rowObj.color;
+    cel4.innerHTML = rowObj.gender;
+}
 
-    const newObj = {
-        pet: pet,
-        name: name,
-        color: color,
-        gender: gender
+function addRowButton() {
+    let pet = document.getElementById('pet').value;
+    let name = document.getElementById('name').value;
+    let color = document.getElementById('color').value;
+    let gender = document.getElementById('gender').value;
+
+    let rowObj = {
+        "pet": pet,
+        "name": name,
+        "color": color,
+        "gender": gender
     };
 
-    tableArray.push(newObj);
-    console.log(tableArray);
+    insertRow(rowObj);
+    tableArray.push(rowObj);
+
 }
 
 function removeRow() {
@@ -119,21 +86,30 @@ function removeRow() {
     table.deleteRow(0);
 
     tableArray.pop();
- }
-
-function sortByName () {
-    tableArray.sort(function (a, b) {
-        const nameA = a.name.toUpperCase();
-        const nameB = b.name.toUpperCase();
-
-        if (nameA > nameB) {
-            return 1;
-        } else if (nameA < nameB) {
-            return -1;
-        } else {
-            return 0;
-        }
-    });
 }
-console.log(tableArray.sort(sortByName()));
+
+function init () {
+    for(let i = 0; i < tableArray.length; i++) {
+        insertRow(tableArray[i]);
+    }
+}
+document.addEventListener('DOMContentLoaded', init);
+console.log(tableArray);
+
+
+// function sortByName () {
+//     tableArray.sort(function (a, b) {
+//         const nameA = a.name.toUpperCase();
+//         const nameB = b.name.toUpperCase();
+//
+//         if (nameA > nameB) {
+//             return 1;
+//         } else if (nameA < nameB) {
+//             return -1;
+//         } else {
+//             return 0;
+//         }
+//     });
+// }
+// console.log(tableArray.sort(sortByName()));
 
